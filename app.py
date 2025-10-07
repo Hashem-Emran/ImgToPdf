4eb4e7f16af9d75b1ca1995b5fe40a7ec81026da
hashem <hashem@.com>
2025-10-07 19:45:01 +0100

    Initial commit: Add ImageToPDFConverter class with basic Tkinter UI setup

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
 
+class ImageToPFConverter:
+    def __init__(self, root):
+        self.root = root
+        self.image_path = []
+        self.output_pdf_name = tk.StringVar()   
+        self.select_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 
-print("Hello, World!")
+        self.instialize_ui()
+
